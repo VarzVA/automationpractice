7e04ee901b7d46ad80b69fb47c1e57899cab74e0
VarzVA <63108965+VarzVA@users.noreply.github.com>
2020-11-19 22:37:13 +0300

    Add methods Add parameters in login method

--- Login.py
+++ Login.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 
-
 wd = webdriver.Chrome('G:\\D\\PyProject\\Drivers\\chromedriver.exe')
 
 
 def open_home_page():
     wd.get("http://automationpractice.com/index.php")
-#wd.set_window_size(1920, 1080)
+
+
+# wd.set_window_size(1920, 1080)
 open_home_page()
+
 
 def login(username="Rifleman59@yandex.ru", password="password"):
     wd.find_element_by_link_text("Sign in").click()
@@ -17,19 +19,35 @@ def login(username="Rifleman59@yandex.ru", password="password"):
     wd.find_element_by_id("SubmitLogin").click()
     wd.implicitly_wait(10)
     wd.find_element_by_link_text("Sign out")
+
+
 login()
 
+
 def add_to_chart_after_login():
-    #go to Woman page
+    # go to Woman page
     wd.find_element_by_class_name("sf-with-ul").click()
     wd.implicitly_wait(10)
-    #change view to "list mode"
+    # change view to "list mode"
     wd.find_element_by_class_name("icon-th-list").click()
+    wd.find_element_by_link_text("Add to cart").click()
+    wd.implicitly_wait(10)
+    wd.find_element_by_link_text("Proceed to checkout").click()
 
-    def list_of_goods():
-        find_list_of_goods = wd.find_element_by_link_text("Add to cart")
-        dress_list = [find_list_of_goods]
-        print(dress_list)
-    list_of_goods()
 
 add_to_chart_after_login()
+
+
+def delete_goods_from_chart():
+    wd.implicitly_wait(10)
+    wd.find_element_by_class_name("cart_quantity_delete").click()
+
+
+delete_goods_from_chart()
+
+# def list_of_goods():
+# find_list_of_goods = wd.find_element_by_link_text("Add to cart")
+# dress_list = [find_list_of_goods]
+# print(dress_list)
+# list_of_goods()
+wd.quit()
